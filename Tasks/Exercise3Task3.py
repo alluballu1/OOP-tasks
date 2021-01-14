93a4f51af5a21de0e9b56dd88d16a061f4ca1d72
Alex Porri <alex.porri@edu.turkuamk.fi>
2021-01-14 09:46:12 +0200

    Create Exercise3Task3.py

---
+++ Tasks/Exercise3Task3.py
@@ -0,0 +1,14 @@
+# File: Exercise3Task3.py
+# Author: Alex Porri
+# Description:  Sort numbers and strings
+
+#List of items
+LIST_OF_NUMBERS = [1,232,34,12312.2323,3213,0,42,7,123,0.1]
+LIST_OF_STRINGS = ['test', 'testi', 'car', 'fruit', 'video', 'jpeg', 'chapter', 'purple', 'travel', 'greenwich']
+
+#Sorting using built-in function
+LIST_OF_NUMBERS.sort()
+LIST_OF_STRINGS.sort()
+
+#Printing out both lists
+print(LIST_OF_STRINGS, LIST_OF_NUMBERS, sep="\n")
