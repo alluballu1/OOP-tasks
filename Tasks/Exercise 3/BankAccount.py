aaaaa686adee3038b126a065cac420267780cf46
Alex Porri <alex.porri@edu.turkuamk.fi>
2021-01-25 13:58:10 +0200

    Exercise examples

--- Tasks/Exercise 3/BankAccount.py
+++ Tasks/Exercise 3/BankAccount.py
@@ -3,14 +3,29 @@ import bank_class
 
 
 def main():
-    print("Hello!")
     # get the starting balance
     start_bal = float(input("Enter the starting balance: "))
+    owner = input("Enter the owner: ")
 
     # create a BankAccount object
-    savings = bank_class.BankAccount(start_bal)
+    savings = bank_class.BankAccount(start_bal, owner)
+    another_ba = ...
 
     # Display the balance
-
     print("Balance is: ", format(savings.get_balance(), ",.2f"), sep="")
+
+    # Get the amount to deposit
+    amount = float(input("Enter the amount to be deposited: "))
+    savings.deposit(amount)
+    print("Balance is: ", format(savings.get_balance(), ",.2f"), sep="")
+
+    #Display the balance
+    amount = float(input("Enter the amount to be withdrawn: "))
+    savings.withdraw(amount)
+    print("Balance is: ", format(savings.get_balance(), ",.2f"), sep="")
+
+    print(savings)
+
+
+
 main()
--- Tasks/Exercise 3/bank_class.py
+++ Tasks/Exercise 3/bank_class.py
@@ -1,18 +1,16 @@
-
 class BankAccount:
     """The __init__ method accepts an argument for the account's balance. It is
     assigned to the __balance attribute"""
-    def __init__(self, bal):
-        self.__balance = bal
 
+    def __init__(self, bal, owner):
+        self.__balance = bal
+        self.__owner = owner
 
     # deposit method
-
     def deposit(self, amount):
         self.__balance += amount
 
     # the withdrawal method
-
     def withdraw(self, amount):
         if self.__balance >= amount:
             self.__balance -= amount
@@ -22,3 +20,13 @@ class BankAccount:
     # the get_balance method returns the account balance
     def get_balance(self):
         return self.__balance
+
+    # the get_owner method
+    def get_owner(self):
+        return self.__owner
+
+    # The __str__ method returns a string
+    # indicating  the object's state
+
+    def __str__(self):
+        return f"""Balance: {self.__balance}  Owner: {self.__owner}"""
